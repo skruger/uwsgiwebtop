7d43196ed55246f4be293fa630f76970d1510122
Shaun Kruger <skruger@neutroninteractive.com>
2015-01-19 14:00:13 -0700

    Initialize project for submission to pypi.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 
+import os
+
 from setuptools import setup
 from pip.req import parse_requirements
 
@@ -9,13 +11,20 @@ install_reqs = parse_requirements("REQUIREMENTS.txt")
 entry_points = {'console_scripts': ['uwsgi-webtop=uwsgiwebtop.cli:main']}
 
 setup(name='uwsgiwebtop',
-      version='0',
+      version='0.1',
       url='https://github.com/skruger/uwsgiwebtop',
       description='Web application to aggregate stats from multiple uWSGI instances',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
       author="Shaun Kruger",
       author_email="shaun.kruger@gmail.com",
       include_package_data=True,
       packages=['uwsgiwebtop'],
       entry_points = entry_points,
       install_requires=[str(ir.req) for ir in install_reqs],
+      license = "BSD",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: BSD License",
+      ],
       )
